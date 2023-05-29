3cbf9fc456b56a591288bb7fe1dea6de261b8176
artyurad <120425795+artyurad@users.noreply.github.com>
2023-05-29 03:13:14 +0300

    Update myprogram.cpp

--- myprogram.cpp
+++ myprogram.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <chrono>
+
+const int ARRAY_SIZE = 10000000;
 
 int main() {
-    // Оптимізації можна застосовувати в цій функції
+    int array[ARRAY_SIZE];
 
-    // Приклад роботи програми без виводу
+    // Ініціалізація масиву
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        array[i] = i;
+    }
+
+    // Обчислення суми елементів масиву
     int sum = 0;
-    for (int i = 0; i < 1000000000; ++i) {
-        sum += i;
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        sum += array[i];
     }
 
     return 0;
